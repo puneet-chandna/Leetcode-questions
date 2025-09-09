d6739d038841649c5a77437f8d7baf968208289a
Puneet Chandna <121252460+puneet-chandna@users.noreply.github.com>
2025-09-09 13:20:50 +0530

    Time: 0 ms (100%), Space: 9.2 MB (84.16%) - LeetHub

---
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        constexpr int MOD = 1'000'000'007;
+        vector<int> dp(n, 0);  
+        dp[0] = 1;
+        long share = 0;
+        for (int i = 1; i < n; ++i) {
+            if (i - delay >= 0) share = (share + dp[i - delay]) % MOD;
+            if (i - forget >= 0) share = (share - dp[i - forget] + MOD) % MOD;
+            dp[i] = share;
+        }
+        long ans = 0;
+        for (int i = n - forget; i < n; ++i)
+            ans = (ans + dp[i]) % MOD;
+        return (int)ans;
+    }
+};

bbb8f39c1b0ac13bb98830090497efb20e324bd7
Puneet Chandna <121252460+puneet-chandna@users.noreply.github.com>
2026-09-19 05:15:39 +0530

    Time: 0 ms (100%), Space: 31.5 MB (7.09%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
+    int ullu(vector<int>& nums, int low, int high, int target){
+        if( low> high) return -1;
+        int mid= (low+high)/2;
+        if(nums[mid]== target)return  mid;
+        else if(target>nums[mid]) return ullu(nums,mid+1,high,target);
+        else if(target<nums[mid])return ullu(nums,low ,mid-1,target);
+        return 0;
+    }
+
     int search(vector<int>& nums, int target) {
         
         int low =0; 
         int high = size(nums)-1;
-        while( low<= high){
-            int mid = (high + low)/2;
-            if( nums[mid] == target){
-                return mid;
-            }
-            else if( target < nums[mid]){
-                high = mid-1;
-                mid = (high + low)/2;
-                }
-
-            else if(target> nums[mid]){
-                low= mid+1;
-                mid = (high + low)/2;
-            }
-        }
         
-        return -1;
+        
+        return ullu(nums,low,high,target);
     }
 };
